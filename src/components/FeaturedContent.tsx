6b69531de967ab61d5c80c336e2485354d0409c9
Lix <zinebbendjafer9@gmail.com>
2023-07-02 19:00:07 +0100

    featuredcontent card styling done

--- src/components/FeaturedContent.tsx
+++ src/components/FeaturedContent.tsx
@@ -5,14 +5,23 @@ const FeaturedContent = () => {
     <div className="font-[Montserrat] h-screen p-20 ">
       <h2 className="text-sky-900 text-[24px] font-bold">FeaturedContent</h2>
 
-      <div className="flex justify-between items-center">
-        <div>
-            <img src={featuredContent[0].image} alt="" />
-            <div>
-                <h2> {featuredContent[0].title} </h2>
-                <p> {featuredContent[0].description} </p>
-            </div>
-        </div>
+      <div className="flex justify-between items-center my-4">
+      <div className="relative group">
+  <img
+    className="filter brightness-50 group-hover:brightness-100 transition-all duration-300 w-80 h-96 cursor-pointer"
+    src={featuredContent[0].image}
+    alt=""
+  />
+  <div className="absolute bottom-0 py-3 px-3 bg-black bg-opacity-0 group-hover:bg-opacity-50">
+    <h2 className="text-white text-[16px] font-semibold">
+      {featuredContent[0].title}
+    </h2>
+    <p className="text-white text-[13px] font-normal">
+      {featuredContent[0].description}
+    </p>
+  </div>
+</div>
+
       </div>
     </div>
   );
