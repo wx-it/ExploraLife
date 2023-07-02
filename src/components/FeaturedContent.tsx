a0bea12d102abbb1f2edfe971c8e921dcd1ad4ef
Lix <zinebbendjafer9@gmail.com>
2023-07-02 20:06:01 +0100

    images swiping using swiper

--- src/components/FeaturedContent.tsx
+++ src/components/FeaturedContent.tsx
@@ -1,27 +1,40 @@
 import featuredContent from "../data/featuredContent";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 const FeaturedContent = () => {
   return (
     <div className="font-[Montserrat] h-screen p-20 ">
       <h2 className="text-sky-900 text-[24px] font-bold">FeaturedContent</h2>
 
       <div className="flex justify-between items-center my-4">
-      <div className="relative group">
-  <img
-    className="filter brightness-50 group-hover:brightness-100 transition-all duration-300 w-80 h-96 cursor-pointer"
-    src={featuredContent[0].image}
-    alt=""
-  />
-  <div className="absolute bottom-0 py-3 px-3 bg-black bg-opacity-0 group-hover:bg-opacity-50">
-    <h2 className="text-white text-[16px] font-semibold">
-      {featuredContent[0].title}
-    </h2>
-    <p className="text-white text-[13px] font-normal">
-      {featuredContent[0].description}
-    </p>
-  </div>
-</div>
-
+        <Swiper
+          spaceBetween={30}
+          slidesPerView={4}
+          navigation
+          loop={true}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          {featuredContent.map((item) => (
+            <SwiperSlide key={item.id}>
+              <div className="relative group w-80 h-96">
+                <img
+                  className="filter brightness-50 group-hover:brightness-100 transition-all duration-300 w-80 h-96 cursor-pointer object-cover"
+                  src={item.image}
+                  alt={item.title}
+                />
+                <div className="absolute bottom-0 py-3 px-3 bg-black bg-opacity-0 group-hover:bg-opacity-50">
+                  <h2 className="text-white text-[16px] font-semibold">
+                    {item.title}
+                  </h2>
+                  <p className="text-white text-[13px] font-normal max-w-xs">
+                    {item.description}
+                  </p>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
--- src/data/featuredContent.ts
+++ src/data/featuredContent.ts
@@ -1,4 +1,5 @@
 interface FeaturedContent {
+  id: number;
   title: string;
   description: string;
   image: string;
@@ -8,82 +9,92 @@ interface FeaturedContent {
 
 const featuredContent: FeaturedContent[] = [
   {
+    id: 1,
     title: "Discover New Places",
     description:
       "Explore breathtaking destinations around the world and create unforgettable memories.",
-    image: "/images/discover-places.jpg",
+    image: "https://source.unsplash.com/featured/?travel",
     buttonText: "Learn More",
     buttonLink: "/explore",
   },
   {
+    id: 2,
     title: "Unleash Your Creativity",
     description:
       "Unlock your creative potential with our innovative tools and resources.",
-    image: "path/to/unleash-creativity.jpg",
+    image: "https://source.unsplash.com/featured/?creativity",
     buttonText: "Get Started",
     buttonLink: "/creativity",
   },
   {
+    id: 3,
     title: "Connect with Like-minded Individuals",
     description:
       "Join our community of passionate individuals and share your interests with others.",
-    image: "path/to/connect-individuals.jpg",
+    image: "https://source.unsplash.com/featured/?community",
     buttonText: "Join Now",
     buttonLink: "/community",
   },
   {
+    id: 4,
     title: "Boost Your Productivity",
     description:
       "Discover effective strategies and tools to enhance your productivity and achieve your goals.",
-    image: "path/to/boost-productivity.jpg",
+    image: "https://source.unsplash.com/featured/?productivity",
     buttonText: "Explore",
     buttonLink: "/productivity",
   },
   {
+    id: 5,
     title: "Learn a New Language",
     description:
       "Embark on a language-learning journey and expand your cultural horizons.",
-    image: "path/to/learn-language.jpg",
+    image: "https://source.unsplash.com/featured/?language",
     buttonText: "Start Learning",
     buttonLink: "/language",
   },
   {
+    id: 6,
     title: "Stay Fit and Healthy",
     description:
       "Discover fitness routines, nutritious recipes, and wellness tips to maintain a healthy lifestyle.",
-    image: "path/to/stay-fit.jpg",
+    image: "https://source.unsplash.com/featured/?fitness",
     buttonText: "Get Active",
     buttonLink: "/fitness",
   },
   {
+    id: 7,
     title: "Master the Art of Photography",
     description:
       "Capture captivating moments and master the techniques of photography.",
-    image: "path/to/photography.jpg",
+    image: "https://source.unsplash.com/featured/?photography",
     buttonText: "Get Inspired",
     buttonLink: "/photography",
   },
   {
+    id: 8,
     title: "Learn to Play a Musical Instrument",
     description:
       "Unleash your musical talent and embark on a melodious journey of learning an instrument.",
-    image: "path/to/learn-instrument.jpg",
+    image: "https://source.unsplash.com/featured/?music",
     buttonText: "Start Playing",
     buttonLink: "/music",
   },
   {
+    id: 9,
     title: "Cook Delicious Recipes",
     description:
       "Delight your taste buds with mouthwatering recipes and culinary adventures.",
-    image: "path/to/cook-recipes.jpg",
+    image: "https://source.unsplash.com/featured/?cooking",
     buttonText: "Try Recipes",
     buttonLink: "/cooking",
   },
   {
+    id: 10,
     title: "Master the Art of Calligraphy",
     description:
       "Learn the timeless art of calligraphy and express your creativity through beautiful lettering.",
-    image: "path/to/calligraphy.jpg",
+    image: "https://source.unsplash.com/featured/?calligraphy",
     buttonText: "Begin Writing",
     buttonLink: "/calligraphy",
   },
