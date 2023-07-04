732593ec264b05f642978d2077f7b4ec2721d51d
Lix <zinebbendjafer9@gmail.com>
2023-07-04 20:48:53 +0100

    better ux

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -3,6 +3,8 @@ import articlesData, {
   Article,
 } from "../data/articlesData";
 
+import { BsArrowUpRight } from "react-icons/bs";
+
 const Articles = () => {
   const {
     lifestyleArticles,
@@ -14,11 +16,19 @@ const Articles = () => {
 
   const renderArticles = (articles: Article[]) => {
     return articles.map((article: Article) => (
-      <div className=" bg-white shadow-3xl p-4 " key={article.id}>
+      <div className=" bg-white" key={article.id}>
         <img className="object-cover" src={article.imageUrl} alt="" />
         <div className="pt-4">
           <h3 className="font-[Montserrat] font-semibold"> {article.title} </h3>
-          <p className="font-[Open Sans] mt-2 text-sm">{article.description}</p>
+          <p className="font-[Open Sans] text-gray-600 mt-2 text-sm">
+            {article.description}
+          </p>
+          <div className="flex gap-2 items-end mt-3">
+            <p className="text-xs font-semibold font-[Montserrat]">
+              Read Article
+            </p>
+            <BsArrowUpRight className='text-sm' />
+          </div>
         </div>
       </div>
     ));
@@ -35,7 +45,7 @@ const Articles = () => {
             <h2 className="text-sky-900 text-[24px] font-bold border-b border-gray-200 mb-6 py-1 ">
               Lifestyle
             </h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 justify-center">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 justify-center">
               {renderArticles(lifestyleArticles)}
             </div>
           </div>
